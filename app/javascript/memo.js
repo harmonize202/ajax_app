329b470b185b5865cad1bc9472b7a9c24698176f
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-07 21:03:49 +0900

    メモ投稿機能のフロントエンド実装

---
+++ app/javascript/memo.js
@@ -0,0 +1,35 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.post;
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // メモとして描画する内容
+      const HTML = `
+        <div class="post" data-id=${item.id}>
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+          ${item.content}
+          </div>
+        </div>`;
+      list.insertAdjacentHTML("afterend", HTML);
+      // メモの入力フォームに入力されたままの文字を削除
+      formText.value = "";
+    };
+    // リロードした際の重複を避ける
+    e.preventDefault();
+  });
+}
+window.addEventListener("load", memo);
