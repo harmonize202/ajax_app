1356a0095cb197a7da07508b4c33ec1762c97bdc
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-07 18:20:55 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,21 @@ class PostsController < ApplicationController
         Post.create(content: params[:content])
         redirect_to action: :index
     end
+
+    def checked
+        post = Post.find(params[:id])
+        # 変数postのcheckedカラムはboolean型の為、真理値が入っている
+        if post.checked 
+            # 既読(true)であれば、解除のためfalseへ
+            post.update(checked: false)
+        else
+            # 未読(false)であれば、既読のためtrueへ
+            post.update(checked: true)
+        end
+    
+        # 更新後の真理値を変数itemへ格納
+        item = Post.find(params[:id])
+        # 変数itemのデータを変数postへ格納し、JSON形式で「checked.js」へ返しレンダリングする。
+        render json: { post: item }
+    end
 end
